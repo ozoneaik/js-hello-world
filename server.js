c47a4cce3521df0cf1e866da6f7c3701c0788e8a
phuwadech <aofphuwadech@gmail.com>
2022-08-27 04:40:55 +0000

    update 2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>สวัสดี my name is aof</h1>')
+  return res.send('<h1>สวัสดี my name is aof joker on the rock</h1>')
 })
 
 const PORT = process.env.PORT || 3000
