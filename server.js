7bcf56c0f0ad4ead883994222dcaa91a2fa8b65a
phuwadech <aofphuwadech@gmail.com>
2022-08-27 04:24:35 +0000

    update message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>สวัสดีเมืองพะเยาจ้า</h1>')
+  return res.send('<h1>สวัสดี my name is aof</h1>')
 })
 
 const PORT = process.env.PORT || 3000
