8a0304a99baf913b3062f9e729cd6ca42a623f1f
phuwadech <aofphuwadech@gmail.com>
2022-08-28 04:48:37 +0000

    edit server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>สวัสดี my name is aof joker on the rock</h1><p>ถ้สเขาจะรักยืนเฉยๆเขาก็รัก</p> <a href="https://www.w3schools.com">Visit W3Schools.com!</a>')
+  return res.send('<h1>สวัสดี my name is aof joker on the rock</h1><p>wdws</p> <a href="https://www.w3schools.com">Visit W3Schools.com!</a>')
 })
 
 const PORT = process.env.PORT || 3000
