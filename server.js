27c51292f810ae00aba135b2e8bd59c9236af8fd
phuwadech <aofphuwadech@gmail.com>
2022-08-28 04:07:44 +0000

    remove aof and aof2 file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>สวัสดี my name is aof joker on the rock</h1> <p>I love you hahmimimi</p>')
+  return res.send('<h1>สวัสดี my name is aof joker on the rock</h1><p>I love you hahmimimi</p>')
 })
 
 const PORT = process.env.PORT || 3000
